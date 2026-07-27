964df71550c379ff6b674052ecb1b4842ebff794
Oulaiya Gaddari <oulaiya.gaddari03@gmail.com>
2026-07-27 22:38:54 +0200

    Update portfolio content to 2026 CV
    - New identity: Oulaiya Gaddari (AI Engineer)
    - Hero, About, Experience (Mantu, Orange/Sofrecom, CDG, Al Barid Bank)
    - Projects: Bank Churn, ML Detection, ENSAM Chatbot, Multi-Agent AI
    - Updated email, LinkedIn; fixed dead links; removed rickroll easter egg

---
+++ precache-manifest.84f6a227952b17e29e335332385c37b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5539619feeeb76d5a7fda8f786e9a7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29673fa3ae0dba46bd77",
+    "url": "/portfolio/static/css/2.62260641.chunk.css"
+  },
+  {
+    "revision": "e06f2e9d7e116256610a",
+    "url": "/portfolio/static/css/main.57011498.chunk.css"
+  },
+  {
+    "revision": "29673fa3ae0dba46bd77",
+    "url": "/portfolio/static/js/2.787345d8.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.787345d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06f2e9d7e116256610a",
+    "url": "/portfolio/static/js/main.10480a46.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
